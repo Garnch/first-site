d2c46d2949d9f7582ad30888de64ceeb6ed1e6c9
Герман <163442104+Garnch@users.noreply.github.com>
2024-08-14 13:25:40 +0300

    the shell of the site

--- p1.py
+++ p1.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello World!'
+    return render_template("inderx.html")
 
 
-@app.route()
+@app.route('/about')
 def about():
-    return 'About page'
+    return render_template('about.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return f"User page: {name} - {id}"
 
 
 if __name__ == '__main__':
